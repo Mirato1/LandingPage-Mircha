617e63db4b6ac862f9035c1a7b4eb29ea09a7420
[Lucas Giménez] <luquetti.5@hotmail.com>
2022-06-30 12:17:17 -0300

    Cambios

--- script.js
+++ script.js
@@ -1,117 +1,113 @@
-import { Cards } from "./components/Cards.js";
-import { MyData } from "./components/MyData.js";
-import { TargetName } from "./components/TargetName.js";
+import { Cards } from './components/Cards.js';
+import { MyData } from './components/MyData.js';
+import { TargetName } from './components/TargetName.js';
 
 // MENÚ
 ((d) => {
-  const $btnMenu = d.querySelector(".header__menu-btn"),
-    $menu = d.querySelector(".nav__menu");
+	const $btnMenu = d.querySelector('.header__menu-btn'),
+		$menu = d.querySelector('.nav__menu');
 
-  $btnMenu.addEventListener("click", (e) => {
-    $menu.classList.toggle("is-active");
-    $btnMenu.classList.toggle("is-active");
-  });
+	$btnMenu.addEventListener('click', (e) => {
+		$menu.classList.toggle('is-active');
+		$btnMenu.classList.toggle('is-active');
+	});
 
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".nav__menu a")) return false;
-    $menu.classList.remove("is-active");
-    $btnMenu.classList.remove("is-active");
-  });
+	d.addEventListener('click', (e) => {
+		if (!e.target.matches('.nav__menu a')) return false;
+		$menu.classList.remove('is-active');
+		$btnMenu.classList.remove('is-active');
+	});
 })(document);
 
 // Formulario de Contacto
-((d) => {
-  const $form = d.querySelector(".contact__form"),
-    $loader = d.querySelector(".contact__form--loader"),
-    $response = d.querySelector(".contact__form--response");
+// ((d) => {
+//   const $form = d.querySelector(".contact__form"),
+//     $loader = d.querySelector(".contact__form--loader"),
+//     $response = d.querySelector(".contact__form--response");
 
-  $form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/luquetti.5@hotmail.com", {
-      method: "POST",
-      body: new FormData(e.target),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((json) => {
-        console.log(json);
-        location.hash = "#gracias";
-        $form.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-        let message =
-          err.statusText || "Ocurrió un error al enviar, intenta nuevamente.";
-        $response.querySelector(
-          "h3"
-        ).innerHTML = `Error ${err.status} : ${message} `;
-      })
-      .finally(() => {
-        $loader.classList.add("none");
-        setTimeout(() => {
-          location.hash = "#close";
-        }, 3000);
-      });
-  });
-})(document);
+//   $form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     $loader.classList.remove("none");
+//     fetch("https://formsubmit.co/ajax/luquetti.5@hotmail.com", {
+//       method: "POST",
+//       body: new FormData(e.target),
+//     })
+//       .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+//       .then((json) => {
+//         console.log(json);
+//         location.hash = "#gracias";
+//         $form.reset();
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         let message =
+//           err.statusText || "Ocurrió un error al enviar, intenta nuevamente.";
+//         $response.querySelector(
+//           "h3"
+//         ).innerHTML = `Error ${err.status} : ${message} `;
+//       })
+//       .finally(() => {
+//         $loader.classList.add("none");
+//         setTimeout(() => {
+//           location.hash = "#close";
+//         }, 3000);
+//       });
+//   });
+// })(document);
 
 //DARK MODE
 ((d) => {
-  // Si tiene el ID se realiza la función
-  if (d.getElementById("dark-mode")) {
-    d.getElementById("dark-mode").addEventListener("change", (e) => {
-      d.documentElement.classList.toggle("dark-mode");
-      let theme;
-      if (d.documentElement.classList.contains("dark-mode")) {
-        theme = "dark";
-      } else {
-        theme = "light";
-      }
-      localStorage.setItem("theme", theme);
-    });
+	// Si tiene el ID se realiza la función
+	if (d.getElementById('dark-mode')) {
+		d.getElementById('dark-mode').addEventListener('change', (e) => {
+			d.documentElement.classList.toggle('dark-mode');
+			let theme;
+			if (d.documentElement.classList.contains('dark-mode')) {
+				theme = 'dark';
+			} else {
+				theme = 'light';
+			}
+			localStorage.setItem('theme', theme);
+		});
 
-    //   Si en el local Storage el fondo es dark se agrega la clase sino no.
-    if (localStorage.getItem("theme") === "dark") {
-      d.documentElement.classList.add("dark-mode");
-    } else {
-      d.documentElement.classList.remove("dark-mode");
-    }
-  }
+		//   Si en el local Storage el fondo es dark se agrega la clase sino no.
+		if (localStorage.getItem('theme') === 'dark') {
+			d.documentElement.classList.add('dark-mode');
+		} else {
+			d.documentElement.classList.remove('dark-mode');
+		}
+	}
 })(document);
 
 // Header Espía
 
 ((d) => {
-  const $sections = d.querySelectorAll("section[data__scroll--spy]");
+	const $sections = d.querySelectorAll('section[data__scroll--spy]');
 
-  const cb = (entries) => {
-    entries.forEach((entry) => {
-      const id = entry.target.getAttribute("id");
+	const cb = (entries) => {
+		entries.forEach((entry) => {
+			const id = entry.target.getAttribute('id');
 
-      if (entry.isIntersecting) {
-        d.querySelector(`a[data__scroll--spy][href="#${id}"]`).classList.add(
-          "spy__on"
-        );
-      } else {
-        d.querySelector(`a[data__scroll--spy][href="#${id}"]`).classList.remove(
-          "spy__on"
-        );
-      }
-    });
-  };
+			if (entry.isIntersecting) {
+				d.querySelector(`a[data__scroll--spy][href="#${id}"]`).classList.add('spy__on');
+			} else {
+				d.querySelector(`a[data__scroll--spy][href="#${id}"]`).classList.remove('spy__on');
+			}
+		});
+	};
 
-  const observer = new IntersectionObserver(cb, {
-    threshold: [0.4, 0.75],
-  });
+	const observer = new IntersectionObserver(cb, {
+		threshold: [0.4, 0.75],
+	});
 
-  $sections.forEach((el) => observer.observe(el));
+	$sections.forEach((el) => observer.observe(el));
 })(document);
 
 //Interactividad Campeones
 ((d) => {
-  d.addEventListener("DOMContentLoaded", (e) => {
-    Cards();
-    MyData();
-    TargetName();
-  });
+	d.addEventListener('DOMContentLoaded', (e) => {
+		Cards();
+		MyData();
+		TargetName();
+	});
 })(document);
