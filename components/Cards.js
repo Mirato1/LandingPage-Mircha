1fdd88d05781c0a9d2a412711b3db6e8ff6fbccf
[Lucas Giménez] <luquetti.5@hotmail.com>
2022-06-30 12:04:56 -0300

    Chau botoncito de CV

--- components/Cards.js
+++ components/Cards.js
@@ -1,62 +1,62 @@
-import api from "../helpers/lol_api.js";
-import { ajax } from "../helpers/ajax.js";
-import champs from "../helpers/champs_runes.js";
+import api from '../helpers/lol_api.js';
+import { ajax } from '../helpers/ajax.js';
+import champs from '../helpers/champs_runes.js';
 
 export function Cards() {
-  let campeon = champs.champions;
-  const apiChamps = api.DATACHAMP;
-  const imgAPI = api.SQUARE;
+	let campeon = champs.champions;
+	const apiChamps = api.DATACHAMP;
+	const imgAPI = api.SQUARE;
 
-  //   Crear un array con los champNick de objetos para poder ordenarlos
-  let nombres = [],
-    titulos = [];
+	//   Crear un array con los champNick de objetos para poder ordenarlos
+	let nombres = [],
+		titulos = [];
 
-  for (let i = 0; i < campeon.length; i++) {
-    nombres.push(campeon[i].nombre);
-    titulos.push([campeon[i].nombre, campeon[i].titulo]);
-  }
-  // Ordenar los nombres alfabeticamente
-  nombres = nombres.sort();
-  titulos = titulos.sort();
+	for (let i = 0; i < campeon.length; i++) {
+		nombres.push(campeon[i].nombre);
+		titulos.push([campeon[i].nombre, campeon[i].titulo]);
+	}
+	// Ordenar los nombres alfabeticamente
+	nombres = nombres.sort();
+	titulos = titulos.sort();
 
-  async function cartas() {
-    for (let i = 0; i < nombres.length; i++) {
-      await ajax({
-        url: `${apiChamps}${nombres[i]}.json`,
-        cbSuccess: (campeones) => {
-          let champsNick = campeones.data,
-            champs = Object.values(champsNick);
-          const $parent = document.querySelector("#champs__section"),
-            $fragment = document.createDocumentFragment();
-          //  Dependiendo la cantidad de nombres que tenga se hace TODO esto
-          for (let i = 0; i < nombres.length; i++) {
-            //  Crear cards dinámicamente según el champ que tengo como objeto
-            let champ = champs.filter((el) => el.id === nombres[i]);
-            for (const el of champ) {
-              const $newCard = document.createElement("a"),
-                $article = document.createElement("article"),
-                $img = document.createElement("img"),
-                $h3 = document.createElement("h3"),
-                $p = document.createElement("p");
-              $newCard.setAttribute("href", "#runas");
-              $article.classList.add("champion__card");
-              $article.setAttribute("id", el.id);
-              $img.setAttribute("src", `${imgAPI}${el.image.full}`);
-              $img.setAttribute("width", "120");
-              $img.setAttribute("alt", `${el.name}`);
-              $h3.textContent = `${el.name}`;
-              $p.textContent = `${titulos[i][1]}`;
-              $article.appendChild($img);
-              $article.appendChild($h3);
-              $article.appendChild($p);
-              $newCard.appendChild($article);
-              $fragment.appendChild($newCard);
-            }
-          }
-          $parent.appendChild($fragment);
-        },
-      });
-    }
-  }
-  cartas();
+	async function cartas() {
+		for (let i = 0; i < nombres.length; i++) {
+			await ajax({
+				url: `${apiChamps}${nombres[i]}.json`,
+				cbSuccess: (campeones) => {
+					let champsNick = campeones.data,
+						champs = Object.values(champsNick);
+					const $parent = document.querySelector('#champs__section'),
+						$fragment = document.createDocumentFragment();
+					//  Dependiendo la cantidad de nombres que tenga se hace TODO esto
+					for (let i = 0; i < nombres.length; i++) {
+						//  Crear cards dinámicamente según el champ que tengo como objeto
+						let champ = champs.filter((el) => el.id === nombres[i]);
+						for (const el of champ) {
+							const $newCard = document.createElement('a'),
+								$article = document.createElement('article'),
+								$img = document.createElement('img'),
+								$h3 = document.createElement('h3'),
+								$p = document.createElement('p');
+							$newCard.setAttribute('href', '#runas');
+							$article.classList.add('champion__card');
+							$article.setAttribute('id', el.id);
+							$img.setAttribute('src', `${imgAPI}${el.image.full}`);
+							$img.setAttribute('width', '120');
+							$img.setAttribute('alt', `${el.name}`);
+							$h3.textContent = `${el.name}`;
+							$p.textContent = `${titulos[i][1]}`;
+							$article.appendChild($img);
+							$article.appendChild($h3);
+							$article.appendChild($p);
+							$newCard.appendChild($article);
+							$fragment.appendChild($newCard);
+						}
+					}
+					$parent.appendChild($fragment);
+				},
+			});
+		}
+	}
+	cartas();
 }
--- components/Skins.js
+++ components/Skins.js
@@ -1,14 +1,14 @@
 export function skins(campeon) {
-  const $splashSection = document.querySelector(".carrousel__slides"),
+  const $splashSection = document.querySelector('.carrousel__slides'),
     $fragment = document.createDocumentFragment();
 
   async function sliders() {
     try {
       const champNick = campeon.nombre;
       const champsAPI =
-          "https://ddragon.leagueoflegends.com/cdn/11.18.1/data/en_US/champion/",
+          'https://ddragon.leagueoflegends.com/cdn/11.18.1/data/en_US/champion/',
         splashAPI =
-          "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
+          'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/';
 
       let promise,
         campeonesData = [],
@@ -22,7 +22,7 @@ export function skins(campeon) {
       }
 
       for (let i = 0; i < campeonesData.length; i++) {
-        skins = await campeonesData[i]["data"][`${champNick}`]["skins"];
+        skins = await campeonesData[i]['data'][`${champNick}`]['skins'];
         for (const key of skins) {
           totalSkins.push(Object.values(key));
         }
@@ -34,18 +34,18 @@ export function skins(campeon) {
       for (const key of numNombre) {
         while (
           $splashSection.firstElementChild.classList.contains(
-            "carrousel__slide"
+            'carrousel__slide'
           )
         ) {
           $splashSection.firstElementChild.remove();
         }
 
-        const $newSlide = document.createElement("div"),
-          $img = document.createElement("img"),
-          $h3 = document.createElement("h3");
-        $newSlide.classList.add("carrousel__slide");
-        $img.setAttribute("src", `${splashAPI}${champNick}_${key[1]}.jpg`);
-        $img.setAttribute("href", `${key[0]}`);
+        const $newSlide = document.createElement('div'),
+          $img = document.createElement('img'),
+          $h3 = document.createElement('h3');
+        $newSlide.classList.add('carrousel__slide');
+        $img.setAttribute('src', `${splashAPI}${champNick}_${key[1]}.jpg`);
+        $img.setAttribute('href', `${key[0]}`);
         $h3.textContent = `${key[0]}`;
         $newSlide.appendChild($img);
         $newSlide.appendChild($h3);
@@ -53,40 +53,40 @@ export function skins(campeon) {
       }
 
       $splashSection.prepend($fragment);
-      $splashSection.firstElementChild.classList.add("active");
+      $splashSection.firstElementChild.classList.add('active');
 
       const d = document;
-      const $nextBtn = d.querySelector(".carrousel__btns .next"),
-        $prevBtn = d.querySelector(".carrousel__btns .prev"),
-        $slides = d.querySelectorAll(".carrousel__slide");
+      const $nextBtn = d.querySelector('.carrousel__btns .next'),
+        $prevBtn = d.querySelector('.carrousel__btns .prev'),
+        $slides = d.querySelectorAll('.carrousel__slide');
 
       let i = 0;
 
-      d.addEventListener("click", (e) => {
+      d.addEventListener('click', (e) => {
         if (e.target === $prevBtn) {
           e.preventDefault();
-          $slides[i].classList.remove("active");
+          $slides[i].classList.remove('active');
           i--;
 
           if (i < 0) {
             i = $slides.length - 1;
           }
 
-          $slides[i].classList.add("active");
+          $slides[i].classList.add('active');
         }
       });
 
-      d.addEventListener("click", (e) => {
+      d.addEventListener('click', (e) => {
         if (e.target === $nextBtn) {
           e.preventDefault();
-          $slides[i].classList.remove("active");
+          $slides[i].classList.remove('active');
           i++;
 
           if (i >= $slides.length) {
             i = 0;
           }
 
-          $slides[i].classList.add("active");
+          $slides[i].classList.add('active');
         }
       });
     } catch (err) {
